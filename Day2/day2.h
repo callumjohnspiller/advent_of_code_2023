d751602f4f4bb6839f93f411ab9b2fa0187e1de0
Callum John Spiller <callumjohnspiller@gmail.com>
2023-12-03 22:44:41 +0000

    Add files via upload

---
+++ Day2/day2.h
@@ -0,0 +1,18 @@
+//
+// Created by Callum Spiller on 2023-12-03.
+//
+
+#ifndef AOC_2023_DAY2_H
+#define AOC_2023_DAY2_H
+
+
+class day2 {
+
+public:
+    static int day_2();
+
+    static int day_2b();
+};
+
+
+#endif //AOC_2023_DAY2_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "Day1/day1.h"
+#include "Day2/day2.h"
 #include "iostream"
 
 int main() {
-    std::cout << day1::day_1() << std::endl;
+//    std::cout << day1::day_1() << std::endl;
+    std::cout << day2::day_2() << std::endl;
+    std::cout << day2::day_2b() << std::endl;
     return 0;
 }
